bdbe8bbb5c333e4bd7fb01622e883beca3b72690
OlhaNester <olga_nv@ukr.net>
2022-01-11 17:17:15 +0200

    ContactForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App">Hello World</div>;
+import { Component } from 'react';
+import ContactForm from './components/ConactForm/CotactForm';
+
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+  handleSubmitForm = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.handleSubmitForm} />
+
+        <h2>Contacts</h2>
+        {/* <Filter ... />
+  <ContactList ... /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ConactForm/CotactForm.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class CotactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+
+  handleChange = event => {
+    const { name, value } = event.currentTarget;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state);
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            Name
+            <input
+              id={this.nameInputId}
+              value={this.state.name}
+              onChange={this.handleChange}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              id={this.numberInputId}
+              value={this.state.number}
+              onChange={this.handleChange}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CotactForm;
